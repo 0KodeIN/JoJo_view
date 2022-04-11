6755765acf4935eb66d73b00168840b5e1bf862d
0KodeIN <it.speed0120@gmail.com>
2022-04-11 21:37:54 +0300

    Добавил тестовое видео

--- script.js
+++ script.js
@@ -27,15 +27,44 @@ sezon1.onclick = function(){
         }
     }
     let n = 24;
+    let d = 0;
     for(let i = 0;i < 24;i++){
         var div = document.createElement('div');
         div.className = "sezon1";
-        div.innerHTML = `<img src="img/1s_c2.png" alt="" class="vue-img"> <p>1 сезон ${n--} серия</p> <button id = "1s${n+1}">Перейти</button> `;
+        d = `1s${n+1}ch`;
+        div.innerHTML = `<img src="img/1s_c2.png" alt="" class="vue-img"> <p>1 сезон ${n--} серия</p> <button id = "1s${n+1}" onclick="createVideo(${n+1})">Перейти</button> `;
         div.id = `1s${n+1}ch`;
         app.insertBefore(div, app.firstChild);
         check = 1;
     }
 
 }
-// sezon.onclick = createVideo();
+
+function createVideo(id){
+    function SetTime(){
+        if(count!=1){
+            alert("Ошибка, ресурс еще в разработке");
+            app.removeChild(app.firstChild);
+            count = 1;
+        }        
+    }
+    var app = document.querySelector('.app');
+    for(let i = 0;i < 24;i++){
+        app.removeChild(app.firstChild)
+    }
+    var count = 0;
+    console.log(id);
+    var div = document.createElement('div');
+    div.className = "video";
+    div.innerHTML = `<video id = "v1"controls src="video/1-${id}.mp4"></video>`;
+    app.insertBefore(div, app.firstChild);
+    check = 0;
+    var aud = document.getElementById("v1");
+    setTimeout(SetTime,1000);
+    aud.onloadeddata = function() {
+        count = 1;  
+    }
+ 
+}
+
 
